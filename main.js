018c470142abb90de85438d4e5607e9125c77545
roger chat <mattrogerpelosi@gmail.com>
2021-08-20 10:26:56 -0400

    commit tree 8a2afe6f353a0978eabaeaec07a668a52439c1af

--- main.js
+++ main.js
@@ -3,9 +3,61 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll('.like-glyph');
+
+hearts.forEach(heart => {
+  heart.addEventListener('click', e => {
+    if(heart.textContent === FULL_HEART) {
+      heart.textContent = EMPTY_HEART;
+      heart.classList.remove('activated-heart');
+    } else {
+    mimicServerCall(e)
+ 
+    .then(status => {if(status === 'Pretend remote server notified of action!'){
+      heart.textContent = FULL_HEART;
+      heart.classList.add('activated-heart');
+    }})
+ 
+    .catch(err => {
+     const error = document.querySelector('div#modal');
+     error.classList.remove('hidden');
+     const p = error.querySelector('p#modal-message');
+     p.textContent = err;
+
+     setTimeout(() => {
+       const error = document.querySelector('div#modal');
+       error.classList.add('hidden');
+     }, 3000);
+
+    })}
+   })
+})
 
 
 
+// for(heart of hearts) {
+//  const eachHeart = document.querySelector('span.like-glyph')
+//   heart.addEventListener('click', e => {
+//    mimicServerCall(e)
+
+//    .then(status => {if(status === 'Pretend remote server notified of action!'){
+//      hearty1.textContent = FULL_HEART;
+//      hearty1.classList.add('activated-heart');
+//    }})
+
+//    .catch(err => {
+//     const error = document.querySelector('div#modal');
+//     error.classList.remove('hidden');
+//     const p = error.querySelector('p#modal-message');
+//     p.textContent = err;
+//     setTimeout(() => {
+//       const error = document.querySelector('div#modal');
+//       error.classList.add('hidden');
+//     }, 3000);})
+ 
+//   })
+// }
+// Pretend remote server notified of action!
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
